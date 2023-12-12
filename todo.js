ddb8e9d189bd991ac7e3874f342e7ff7936c813f
Ines Zenkri <ines@zenkri.com>
2023-12-12 17:07:02 +0100

    added side bar for done tasks

--- todo.js
+++ todo.js
@@ -1,23 +1,27 @@
-// Function to load tasks from localStorage when the page loads
+
+let completedTasks = JSON.parse(localStorage.getItem('completedTasks')) || [];
+
 window.onload = function() {
   loadTasks();
+  populateSidebar(); 
 };
 
 function loadTasks() {
   var taskList = document.getElementById("taskList");
   var tasks = localStorage.getItem("tasks");
-  
+
   if (tasks) {
     taskList.innerHTML = tasks;
     var listItems = document.querySelectorAll("#taskList li");
-    
+
     listItems.forEach(function(li) {
       li.onclick = function() {
         if (!li.classList.contains("checked")) {
           li.classList.add("checked");
+          markTaskAsCompleted(li);
           setTimeout(function() {
             li.remove();
-            saveTasks(); 
+            saveTasks();
           }, 4000);
         }
       };
@@ -28,6 +32,36 @@ function loadTasks() {
 function saveTasks() {
   var taskList = document.getElementById("taskList");
   localStorage.setItem("tasks", taskList.innerHTML);
+}
+
+function markTaskAsCompleted(taskElement) {
+  const taskName = taskElement.querySelector("span:last-child").innerText;
+  const timestamp = new Date().toLocaleString();
+  
+  const taskDetails = {
+    taskName: taskName,
+    timestamp: timestamp
+  };
+
+  completedTasks.push(taskDetails);
+  saveCompletedTasks();
+}
+
+function saveCompletedTasks() {
+  localStorage.setItem('completedTasks', JSON.stringify(completedTasks));
+}
+
+function populateSidebar() {
+  const sidebar = document.getElementById('sidebar');
+
+  const historyHTML = completedTasks.map(task => {
+    return `<div class="history-item">
+              <p>${task.taskName}</p>
+              <p class="timestamp">${task.timestamp}</p>
+            </div>`;
+  }).join('');
+
+  sidebar.innerHTML = historyHTML;
 }
 
 function addTask() {
@@ -50,7 +84,7 @@ function addTask() {
     alert("Please enter a task!");
   } else {
     taskList.appendChild(li);
-    saveTasks(); 
+    saveTasks();
   }
 
   taskInput.value = "";
@@ -58,10 +92,20 @@ function addTask() {
   li.onclick = function() {
     if (!li.classList.contains("checked")) {
       li.classList.add("checked");
+      markTaskAsCompleted(li);
       setTimeout(function() {
         li.remove();
         saveTasks();
       }, 4000);
     }
   };
+}
+
+function toggleSidebar() {
+  const sidebar = document.getElementById('sidebar');
+  sidebar.classList.toggle('show'); 
+
+  if (sidebar.classList.contains('show')) {
+    populateSidebar(); 
+  }
 }
